fffc9ff27a17a5fdee9682935441e758d5fa428b
drev4 <diegorevhoz4@gmail.com>
2018-04-17 16:19:51 +0200

    Eliminado directorio duplicado

--- apiMongoDB/model/sport.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gopkg.in/mgo.v2/bson"
-
-//Sport Objeto deporte
-type Sport struct {
-	ID   bson.ObjectId `json:"id,omitempty"`
-	Name string        `json:"firstname,omitempty"`
-}
